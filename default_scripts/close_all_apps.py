bcd3915e4b657a6d6312d31b145d1b76631bf365
Антон <antonbliznuk71@gmail.com>
2025-11-23 15:59:57 +0000

    implemented default script

---
+++ default_scripts/close_all_apps.py
@@ -0,0 +1,24 @@
+from controls.app import AppController
+
+
+def close_all_apps() -> None:
+    app_controller = AppController()
+    app_controller.close_every_application_except([
+        "Finder",
+        "Ghostty",
+        "Aerospace",
+        "AlDente",
+        "System Events",
+        "Dock",
+        "NotificationCenter",
+    ])
+
+    app_controller.force_close_applications([
+        "Steam",
+        "Discord"
+    ])
+
+
+if __name__ == "__main__":
+    close_all_apps()
+
