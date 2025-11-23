cc6fdd05a6d2d0a058e070e071a7710b689b3f33
Антон <antonbliznuk71@gmail.com>
2025-11-23 18:13:52 +0000

    implemented planing script which turns on planing application, implemented close planing script which closes planing applications.

---
+++ default_scripts/planing.py
@@ -0,0 +1,11 @@
+from controls.app import AppController
+
+def planing_func() -> None:
+    app_controller = AppController()
+    app_controller.open_applications([
+        "Reminders",
+        "Calendar"
+    ])
+
+if __name__ == "__main__":
+    planing_func()
