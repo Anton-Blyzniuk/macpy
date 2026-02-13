5b90fcd70a72af84afdf4216856b7a1332f9a170
Антон <antonbliznuk71@gmail.com>
2026-02-13 21:23:37 +0000

    Implemented desktop controller

--- macpy/controllers/__init__.py
+++ macpy/controllers/__init__.py
@@ -2,3 +2,5 @@ from macpy.controllers.audio import AudioController as AudioController
 from macpy.controllers.network import NetworkController as NetworkController
 from macpy.controllers.display import DisplayController as DisplayController
 from macpy.controllers.power import PowerController as PowerController
+from macpy.controllers.apps import AppController as AppController
+from macpy.controllers.desktop import DesktopController as DesktopController
---
+++ macpy/controllers/desktop.py
@@ -0,0 +1,20 @@
+from macpy.core import BaseController, CommandResult
+
+
+class DesktopController(BaseController):
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+        self.current_wallpaper: str|None = None
+
+    def set_wallpaper(self, path_to_image: str) -> CommandResult:
+        return self._execute(
+            [
+                "osascript",
+                "-e",
+                f'tell application "System Events" to set picture of every desktop to "{path_to_image}"'
+            ],
+            restart_dock=True,
+            raise_on_error=True
+        )
