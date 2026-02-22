1cdd074129c8155112031b0eb14b97e3dc012e9e
Антон <antonbliznuk71@gmail.com>
2026-02-22 10:15:59 +0000

    Implemented create_script flow.

--- macpy/cli/flow.py
+++ macpy/cli/flow.py
@@ -1,26 +1,32 @@
-from typing import Any
 from macpy.cli import CLIMenu
+from macpy import controllers, generation
 
 
 class CLIInterface:
     @staticmethod
-    def show_list_and_select_element(
-            list_element: list,
-            message: str,
-            exit_word: str,
-            confirm_action: bool = False
-    ) -> Any:
+    def create_script_flow(script_name: str) -> str:
+        usage_units = []
+        controllers_names = [c.__name__ for c in controllers.__available_controllers__]
         while True:
-            CLIMenu.show_list(list_element)
-            try:
-                result = CLIMenu.select_element_from_list(
-                    message=message,
-                    arg_list=list_element,
-                    exit_word=exit_word
+            choice = CLIMenu.show_list_and_select_element(
+                list_element=controllers_names,
+                message=f"Select controller: ",
+                exit_word="exit",
+                confirm_action=True,
+            )
+            if choice == "exit":
+                break
+
+            usage_units.append(
+                CLIMenu.get_controller_usage_unit(
+                    controllers.__available_controllers__[choice],
                 )
-                if confirm_action:
-                    if not CLIMenu.confirm_action("Are you sure? [y/N]: "):
-                        continue
-                return result
-            except ValueError as error:
-                CLIMenu.show_error(error=error, message="Element not found.")
+            )
+
+        return generation.ScriptBuilder.build_script(
+            script_name=script_name,
+            controller_usage_units=usage_units,
+        )
+
+if __name__ == "__main__":
+    print(CLIInterface.create_script_flow("new_script"))
--- macpy/cli/menu.py
+++ macpy/cli/menu.py
@@ -1,5 +1,7 @@
 from macpy.cli import CLIInput
+from typing import get_origin, get_args
 from typing import Any
+from macpy import controllers, generation, inspection
 
 class CLIMenu:
 
@@ -12,16 +14,6 @@ class CLIMenu:
     def show_error(error: Exception, message: str) -> None:
         print(f"{message} -> {error}")
 
-    @staticmethod
-    def select_element_from_list(arg_list: list, message: str, exit_word: str) -> Any:
-        return arg_list[
-            CLIInput.input_argument(
-                arg_type=int,
-                message=message,
-                validator=lambda value: value in range(len(arg_list)),
-                exit_word=exit_word
-            )
-        ]
 
     @staticmethod
     def confirm_action(question: str) -> bool:
@@ -29,4 +21,102 @@ class CLIMenu:
             arg_type=str,
             message=question,
         ).lower() == "y"
+
+    @staticmethod
+    def show_list_and_select_element(
+            list_element: list,
+            message: str,
+            exit_word: str,
+            confirm_action: bool = False
+    ) -> Any:
+        while True:
+            CLIMenu.show_list(list_element)
+            try:
+                result = CLIInput.input_argument(
+                    arg_type=int,
+                    message=message,
+                    exit_word=exit_word,
+                )
+                if confirm_action:
+                    if not CLIMenu.confirm_action("Are you sure? [y/N]: "):
+                        continue
+                return result
+            except ValueError as error:
+                CLIMenu.show_error(error=error, message="Element not found.")
+
+
+    @staticmethod
+    def get_method_values(method) -> dict:
+        values = {}
+        for p in method.get("parameters", []):
+            annotation = p.get("annotation")
+            if annotation in [int, float, str, bool]:
+                values[p.get("name")] = CLIInput.input_argument(
+                    arg_type=annotation,
+                    message=f"Method: {method.get('name')} requires argument ({p.get('name')} -> {annotation}): "
+                )
+            elif get_origin(annotation) is list:
+                values[p.get("name")] = CLIInput.input_list(
+                    arg_type=get_args(annotation)[0],
+                    message=f"Method {method.get('name')} requires list ({p.get('name')} -> {get_args(annotation)[0]}): "
+                )
+        return values
+
+    @staticmethod
+    def get_method_calls(methods) -> list[str]:
+        result = []
+        for method in methods:
+            result.append(
+                generation.CallGenerator.generate_method_call_code(
+                    inspected_method=method,
+                    variable_name=controllers.AppController.__name__.lower(),
+                    values=CLIMenu.get_method_values(
+                        method
+                    ),
+                )
+            )
+        return result
+
+    @staticmethod
+    def get_controller_usage_unit(controller_class) -> str:
+        method_calls = []
+        inspected_methods = inspection.Inspector.get_methods(controller_class)
+        method_names = [method.get("name") for method in inspected_methods]
+        while True:
+            choice = CLIMenu.show_list_and_select_element(
+                list_element=method_names,
+                message=f"Select method: ",
+                exit_word="exit",
+                confirm_action=False,
+            )
+            if choice == "exit":
+                break
+            method = inspected_methods[choice]
+            method_calls.append(
+                generation.CallGenerator.generate_method_call_code(
+                    inspected_method=method,
+                    variable_name=controller_class.__name__.lower(),
+                    values=CLIMenu.get_method_values(
+                        method=method
+                    )
+                )
+            )
+
+        return generation.ScriptBuilder.build_controller_usage_unit(
+            generation.CallGenerator.generate_constructor_code(
+                inspected_constructor=inspection.Inspector.get_constructor(controller_class),
+                class_name=controller_class.__name__,
+                variable_name=controller_class.__name__.lower(),
+                values=CLIMenu.get_method_values(
+                    method=inspection.Inspector.get_constructor(controller_class)
+                )
+            ),
+            method_calls=method_calls,
+            comment=f"{controller_class.__name__} usage unit:"
+        )
+
+
+if __name__ == "__main__":
+    print(CLIMenu.get_controller_usage_unit(controller_class=controllers.DisplayController))
+
 
