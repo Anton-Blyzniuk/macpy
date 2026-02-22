a85339fb0d2ee4d203602ceb92d625849822e150
Антон <antonbliznuk71@gmail.com>
2026-02-22 12:12:54 +0000

    added new setting in settings.py

--- macpy/settings.py
+++ macpy/settings.py
@@ -1,5 +1,14 @@
+import os
+
+# Path to settings file
+SETTINGS_FILE_PATH = os.path.join(
+    os.path.dirname(os.path.dirname(os.path.abspath(__file__))),
+    "macpy",
+    "settings.py",
+)
+
 # Display controller settings
 DISPLAY_ID = "37D8832A-2D66-02CA-B9F7-8F30A301B230"
 
 # Examples: code, nvim, vim, nano
-CODE_EDITOR_COMMAND = "code"
+CODE_EDITOR_COMMAND = "nvim"
