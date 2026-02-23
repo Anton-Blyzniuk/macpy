9f05c7a3739c5297034827138046fd862ada77f7
Антон <antonbliznuk71@gmail.com>
2026-02-23 12:49:39 +0000

    connected output class

--- macpy/cli/__init__.py
+++ macpy/cli/__init__.py
@@ -1,3 +1,4 @@
 from macpy.cli.input import CLIInput as CLIInput
+from macpy.cli.output import CLIOutput as CLIOutput
 from macpy.cli.menu import CLIMenu as CLIMenu
 from macpy.cli.flow import CLIInterface as CLIInterface
